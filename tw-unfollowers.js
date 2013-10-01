5c435dfcba38cde5ef2d71b304052c8e34f1fda5
Fry Falavel <fry@justgomusic.com>
2013-10-01 16:48:36 +0100

    twitter version

--- tw-unfollowers.js
+++ tw-unfollowers.js
@@ -42,7 +42,15 @@
  *
  * USAGE:   run this script when you are on http://followback.me/explore/*
  */
+
+http://code.jquery.com/jquery-1.10.2.min.js
+
 (function () {
+
+    var script = document.createElement('script');
+    script.src = 'http://code.jquery.com/jquery-1.10.2.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
 
     function main() {
          console.log($("#users_list li").text());
