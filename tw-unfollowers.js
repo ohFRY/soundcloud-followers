87ea8d009151a2a695a461b4c48e36d7354d66c1
Fry Falavel <fry@justgomusic.com>
2013-10-02 01:06:30 +0100

    twitter version

--- tw-unfollowers.js
+++ tw-unfollowers.js
@@ -60,7 +60,7 @@
             var a = document.createElement('a');
             a.href = 'http://code.jquery.com/jquery-1.10.2.min.js';
             a.class = '.btn-success'; 
-            a.appendChild(document.createTextNode(value.text()));
+            a.appendChild(document.createTextNode(value));
             $('.pull-right').get(key).appendChild(a);
         });
 
